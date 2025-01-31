aae52197467246d389a05c8f1565e18e78b1ce0f
sobaman <sobaman522@gmail.com>
2025-01-31 17:02:07 +0900

    feat: hashtag 등록 기능 구현
    - 해시태그가 DB에 존재하지 않을때만 생성
    - 해시태그 content 로 부터 단건 조회하는 로직 생성

---
+++ backend/src/main/java/com/example/backend/content/hashtag/service/HashtagService.java
@@ -0,0 +1,37 @@
+package com.example.backend.content.hashtag.service;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.stereotype.Service;
+
+import com.example.backend.content.hashtag.exception.HashtagErrorCode;
+import com.example.backend.content.hashtag.exception.HashtagException;
+import com.example.backend.entity.HashtagEntity;
+import com.example.backend.entity.HashtagRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class HashtagService {
+
+	private final HashtagRepository hashtagRepository;
+
+	public HashtagEntity createIfNotExists(String content) {
+		return hashtagRepository.findByContent(content)
+			.orElseGet(() -> hashtagRepository.save(
+				HashtagEntity.builder()
+					.content(content)
+					.build()
+			));
+	}
+
+	public HashtagEntity findByContent(String content) {
+		return hashtagRepository.findByContent(content)
+			.orElseThrow(() -> new HashtagException(HashtagErrorCode.NOT_FOUND));
+	}
+
+}
