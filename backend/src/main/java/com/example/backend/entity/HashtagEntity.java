0782c6d8d18f7c5bea56798f28916ac26e5f4daa
sobaman <sobaman522@gmail.com>
2025-01-31 16:58:31 +0900

    feat: hashtag 엔티티에 유효성 검사 로직 추가
    - 해시태그의 길이는 10글자 이하
    - 영문,숫자,한글,_ 만 가능

--- backend/src/main/java/com/example/backend/entity/HashtagEntity.java
+++ backend/src/main/java/com/example/backend/entity/HashtagEntity.java
@@ -1,5 +1,10 @@
 package com.example.backend.entity;
 
+import java.util.regex.Pattern;
+
+import com.example.backend.content.hashtag.exception.HashtagErrorCode;
+import com.example.backend.content.hashtag.exception.HashtagException;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -19,10 +24,32 @@ import lombok.NoArgsConstructor;
 @Builder
 @Table(name = "hashtag")
 public class HashtagEntity {
+
+	private static final Pattern HASHTAG_PATTERN = Pattern.compile("^[\\w가-힣]+$");
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(unique = true, nullable = false)
 	private String content;
+
+	public HashtagEntity(String content) {
+		validateContent(content);
+		this.content = content;
+	}
+
+	private void validateContent(String content) {
+
+		if (content == null || content.isBlank()) {
+			throw new HashtagException(HashtagErrorCode.EMPTY_CONTENT);
+		}
+		if (content.length() > 10) {
+			throw new HashtagException(HashtagErrorCode.TOO_LONG_HASHTAG_CONTENT);
+		}
+		if (!HASHTAG_PATTERN.matcher(content).matches()) {
+			throw new HashtagException(HashtagErrorCode.INVALID_HASHTAG_CONTENT);
+		}
+
+	}
 }
