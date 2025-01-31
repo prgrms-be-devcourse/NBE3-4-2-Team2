c7af2e2bc8b7e3e040b4e14e47cf7ac8859e9824
joonaeng <jylee33286315@gmai.com>
2025-01-31 14:27:25 +0900

    feat: PostCreate 구현

--- backend/src/main/java/com/example/backend/content/post/service/PostService.java
+++ backend/src/main/java/com/example/backend/content/post/service/PostService.java
@@ -1,20 +1,16 @@
 package com.example.backend.content.post.service;
 
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.backend.content.post.dto.PostCreateRequest;
 import com.example.backend.content.post.dto.PostCreateResponse;
-import com.example.backend.content.post.dto.PostDeleteResponse;
-import com.example.backend.content.post.dto.PostModifyRequest;
-import com.example.backend.content.post.dto.PostModifyResponse;
-import com.example.backend.content.post.exception.PostErrorCode;
-import com.example.backend.content.post.exception.PostException;
 import com.example.backend.entity.MemberEntity;
 import com.example.backend.entity.MemberRepository;
 import com.example.backend.entity.PostEntity;
 import com.example.backend.entity.PostRepository;
+import com.example.backend.global.error.GlobalErrorCode;
+import com.example.backend.global.exception.GlobalException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -36,46 +32,18 @@ public class PostService {
 	 *
 	 * @param request 게시물 생성 요청 객체
 	 * @return PostCreateResponse 객체
-	 * @throws PostException 존재하지 않는 회원일 경우 예외 발생
+	 * @throws IllegalArgumentException 존재하지 않는 회원일 경우 예외 발생
 	 */
 	@Transactional
 	public PostCreateResponse createPost(PostCreateRequest request) {
 		MemberEntity memberEntity = memberRepository.findById(request.getMemberId())
-			.orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 회원입니다."));
-		//MEMBER 클래스 EXCEPTION 으로 변경 예정
+			.orElseThrow(() -> new GlobalException(GlobalErrorCode.VALIDATION_FAILED));
+
 		PostEntity postEntity = request.toEntity(memberEntity);
 
 		PostEntity savedPost = postRepository.save(postEntity);
 
 		return PostCreateResponse.fromEntity(savedPost);
-	}
-
-	@Transactional
-	public PostModifyResponse modifyPost(Long postId, PostModifyRequest request) {
-		PostEntity postEntity = postRepository.findById(postId)
-			.orElseThrow(() -> new PostException(PostErrorCode.POST_NOT_FOUND));
-
-		if (!postEntity.getMember().getId().equals(request.getMemberId())) {
-			throw new PostException(PostErrorCode.POST_UPDATE_FORBIDDEN);
-		}
-
-		postEntity.modifyContent(request.getContent());
-
-		return PostModifyResponse.fromEntity(postEntity);
-	}
-
-	@Transactional
-	public PostDeleteResponse deletePost(Long postId, Long memberId) {
-		PostEntity postEntity = postRepository.findByIdAndIsDeletedFalse(postId)
-			.orElseThrow(() -> new PostException(PostErrorCode.POST_NOT_FOUND));
-
-		if (!postEntity.getMember().getId().equals(memberId)) {
-			throw new PostException(PostErrorCode.POST_DELETE_FORBIDDEN);
-		}
-
-		postEntity.deleteContent();
-
-		return PostDeleteResponse.fromEntity(postId);
 	}
 
 }
