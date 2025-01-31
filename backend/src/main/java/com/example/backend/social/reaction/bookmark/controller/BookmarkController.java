e7430bb487ec88726e5fd3487f1edb7d54a661c3
Metronon <bumpercar07@gmail.com>
2025-01-31 09:48:17 +0900

    feat: controller 임시 작성

---
+++ backend/src/main/java/com/example/backend/social/reaction/bookmark/controller/BookmarkController.java
@@ -0,0 +1,16 @@
+package com.example.backend.social.reaction.bookmark.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * BookmarkController
+ * "/bookmark" 로 들어오는 요청 처리 컨트롤러
+ * TODO : postService, memberService가 구현되면 로직 작성 예정
+ * @author Metronon
+ * @since 2025-01-31
+ */
+@RestController
+@RequestMapping("/api-v1/bookmark")
+public class BookmarkController {
+}
