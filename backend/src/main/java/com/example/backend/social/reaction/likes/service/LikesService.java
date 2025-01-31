6b9d61bf2c3330fb13b6411515b1fb3b8673d411
Metronon <bumpercar07@gmail.com>
2025-01-31 12:21:45 +0900

    refactor: Service create DTO 반환 & 주석 추가

--- backend/src/main/java/com/example/backend/social/reaction/likes/service/LikesService.java
+++ backend/src/main/java/com/example/backend/social/reaction/likes/service/LikesService.java
@@ -10,6 +10,7 @@ import com.example.backend.entity.MemberEntity;
 import com.example.backend.entity.MemberRepository;
 import com.example.backend.entity.PostEntity;
 import com.example.backend.entity.PostRepository;
+import com.example.backend.social.reaction.likes.dto.LikesResponse;
 import com.example.backend.social.reaction.likes.exception.LikesErrorCode;
 import com.example.backend.social.reaction.likes.exception.LikesException;
 
@@ -33,21 +34,43 @@ public class LikesService {
 		this.postRepository = postRepository;
 	}
 
+	/**
+	 * 좋아요 생성 메서드
+	 * memberId와 postId를 받아 LikesEntity 생성
+	 *
+	 * @param memberId
+	 * @param postId
+	 * @return LikesResponse (DTO)
+	 */
 	@Transactional
-	public LikesEntity createLike(Long memberId, Long postId) {
+	public LikesResponse createLike(Long memberId, Long postId) {
 		MemberEntity member = memberRepository.findById(memberId)
 			.orElseThrow(() -> new LikesException(LikesErrorCode.MEMBER_NOT_FOUND));
 		PostEntity post = postRepository.findById(postId)
 			.orElseThrow(() -> new LikesException(LikesErrorCode.POST_NOT_FOUND));
 
-		LikesEntity like = new LikesEntity(member, post);
-		return likesRepository.save(like);
+		LikesEntity like = LikesEntity.builder()
+			.member(member)
+			.post(post)
+			.build();
+
+		likesRepository.save(like);
+
+		return LikesResponse.toResponse(like);
 	}
 
+	/**
+	 * 좋아요 취소 메서드
+	 * memberId와 postId를 받아 LikesEntity 삭제
+	 *
+	 * @param memberId
+	 * @param postId
+	 */
 	@Transactional
 	public void deleteLike(Long memberId, Long postId) {
 		LikesEntity like = likesRepository.findByMemberIdAndPostId(memberId, postId)
 				.orElseThrow(() -> new LikesException(LikesErrorCode.LIKE_NOT_FOUND));
+
 		likesRepository.deleteByMemberIdAndPostId(memberId, postId);
 	}
 }
