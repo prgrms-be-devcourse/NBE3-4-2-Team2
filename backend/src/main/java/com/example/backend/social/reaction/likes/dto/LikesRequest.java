4b0e12e24aafdf68a783212d843b45a1f4fd6d93
Metronon <bumpercar07@gmail.com>
2025-02-01 21:34:44 +0900

    feature: add DTO Validation

--- backend/src/main/java/com/example/backend/social/reaction/likes/dto/LikesRequest.java
+++ backend/src/main/java/com/example/backend/social/reaction/likes/dto/LikesRequest.java
@@ -1,5 +1,6 @@
 package com.example.backend.social.reaction.likes.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,6 +16,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class LikesRequest {
+	@NotNull(message = "Member Id는 필수 항목입니다.")
 	private Long memberId;
+
+	@NotNull(message = "Post Id는 필수 항목입니다.")
 	private Long postId;
 }
