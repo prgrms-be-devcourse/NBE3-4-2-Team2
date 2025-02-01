7fe61a69dbddbc25765aab4d4bfdc6955eda8e6a
ChoiHyunSan <chsan626@gmail.com>
2025-02-01 21:59:06 +0900

    feat: Feed 요청정보에 대한 체크를 위해 유효성 검사 역할을 맡는 FeedValidator 클래스 구현 및 ErrorCode 정의

---
+++ backend/src/main/java/com/example/backend/social/feed/implement/FeedValidator.java
@@ -0,0 +1,42 @@
+package com.example.backend.social.feed.implement;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Component;
+
+import com.example.backend.global.exception.GlobalException;
+import com.example.backend.social.feed.dto.FeedRequest;
+import com.example.backend.social.feed.exception.FeedErrorCode;
+
+/***
+ * FeedValidator
+ * 피드 요청 정보의 유효성을 검사하는 클래스
+ * @author ChoiHyunSan
+ * @since 2025-02-01
+ */
+@Component
+public class FeedValidator {
+
+	/**
+	 * Feed 를 요청하는 경우에 호출하여 Request 정보가 유효한지 확인하는 메서드
+	 * @param request Feed 요청 정보
+	 */
+	public void validateRequest(FeedRequest request) {
+
+		// 1. 요청 시간 ( RequestTime )
+		// - 요청시간이 현재 시간보다 큰 경우
+		LocalDateTime requestTime = request.getTimestamp();
+		if (requestTime.isAfter(LocalDateTime.now())) {
+			throw new GlobalException(FeedErrorCode.WRONG_TIMESTAMP_REQUEST);
+		}
+
+		// 2. 요청 최대 개수
+		// - 0개 이하의 개수를 요청하는 경우
+		// - 20개 초과의 개수를 요청하는 경우
+		Integer maxSize = request.getMaxSize();
+		if (maxSize <= 0 || maxSize > 20) {
+			throw new GlobalException(FeedErrorCode.WRONG_MAXSIZE_REQUEST);
+		}
+		
+	}
+}
