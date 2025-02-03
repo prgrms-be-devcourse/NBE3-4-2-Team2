084a36a20247b268d0a87e73cb5d99d75eded635
sobaman <sobaman522@gmail.com>
2025-02-03 09:33:36 +0900

    refactor: 에러코드 오타 수정

--- backend/src/main/java/com/example/backend/content/hashtag/exception/HashtagErrorCode.java
+++ backend/src/main/java/com/example/backend/content/hashtag/exception/HashtagErrorCode.java
@@ -12,7 +12,7 @@ import lombok.Getter;
 public enum HashtagErrorCode implements ErrorCodeIfs {
 
 	INVALID_HASHTAG_CONTENT(HttpStatus.BAD_REQUEST, 400, "해시태그 특수문자를 포함할 수 없습니다."),
-	EMPTY_CONTENT(HttpStatus.BAD_REQUEST, 400, "해시태크 내용이 없습니다."),
+	EMPTY_CONTENT(HttpStatus.BAD_REQUEST, 400, "해시태그 내용이 없습니다."),
 	TOO_LONG_HASHTAG_CONTENT(HttpStatus.BAD_REQUEST, 400, "해시태그 길이가 10자를 초과합니다."),
 	NOT_FOUND(HttpStatus.NOT_FOUND, 404, "해당 데이터를 찾을 수 없습니다.");
 
