77bb63895293f4199ced3ebbc4e1f56dfeba0691
sobaman <sobaman522@gmail.com>
2025-02-03 15:51:24 +0900

    feat: PostHashtagEntity 에 isDeleted 필드 추가

--- backend/src/main/java/com/example/backend/entity/PostHashtagEntity.java
+++ backend/src/main/java/com/example/backend/entity/PostHashtagEntity.java
@@ -1,5 +1,6 @@
 package com.example.backend.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -32,4 +33,8 @@ public class PostHashtagEntity {
 	@JoinColumn(nullable = false, name = "hashtag_id")
 	@ManyToOne(fetch = FetchType.LAZY)
 	HashtagEntity hashtag;
+
+	@Column(nullable = false)
+	private boolean isDeleted;
+
 }
