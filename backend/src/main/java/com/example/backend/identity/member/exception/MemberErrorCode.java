b84c2ddc6f0b0c0e8d32bb16cb10768a3e404e39
k-haechan <gibbm1127@naver.com>
2025-02-03 14:29:27 +0900

    add : MemberErrorCode 추가

--- backend/src/main/java/com/example/backend/identity/member/exception/MemberErrorCode.java
+++ backend/src/main/java/com/example/backend/identity/member/exception/MemberErrorCode.java
@@ -12,7 +12,10 @@ import lombok.Getter;
 public enum MemberErrorCode implements ErrorCodeIfs {
 
 	INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, 401, "비밀번호가 일치하지 않습니다."),
-	NOT_FOUND(HttpStatus.NOT_FOUND, 404, "사용자 정보가 존재하지 않습니다.");
+	NOT_FOUND(HttpStatus.NOT_FOUND, 404, "사용자 정보가 존재하지 않습니다."),
+	CONFLICT_RESOURCE(HttpStatus.CONFLICT, 409, "해당 사용자가 이미 존재합니다.");
+	// NOT_FOUND(HttpStatus.NOT_FOUND, 404, "해당 데이터를 찾을 수 없습니다."),
+
 
 	private final HttpStatus httpStatus;
 	private final Integer code;
