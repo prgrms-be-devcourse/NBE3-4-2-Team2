c3c9b81490937b030575e0917c0a6f24c86d27fb
sobaman <sobaman522@gmail.com>
2025-02-03 18:59:44 +0900

    feat: 사용한지 3개월 이상된 hashtag Id 조회 및 ids 기반으로 Hashtag 벌크성 삭제 쿼리 추가

--- backend/src/main/java/com/example/backend/entity/HashtagRepository.java
+++ backend/src/main/java/com/example/backend/entity/HashtagRepository.java
@@ -1,6 +1,7 @@
 package com.example.backend.entity;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -18,11 +19,28 @@ public interface HashtagRepository extends JpaRepository<HashtagEntity, Long> {
 	 * @author kwak
 	 * @since 2025-02-03
 	 */
-	@Modifying
+	@Modifying(clearAutomatically = true)
 	@Query("""
 		UPDATE HashtagEntity h
 		SET h.lastUsedAt = :now
 		WHERE h.id IN :hashtagUsageData
 		""")
 	void bulkLastUsedAt(@Param("hashtagUsageData") Set<Long> hashtagUsageData, @Param("now") LocalDateTime now);
+
+	/**
+	 * ids 기반으로 벌크성 삭제 쿼리
+	 * @author kwak
+	 * @since 2025-02-03
+	 */
+	@Modifying(clearAutomatically = true)
+	@Query("DELETE FROM HashtagEntity h WHERE h.id IN :ids")
+	int bulkDeleteByIds(@Param("ids") List<Long> ids);
+
+	/**
+	 * 사용한지 3개월 이상 된 hashtag Id 조회
+	 * @author kwak
+	 * @since 2025-02-03
+	 */
+	@Query("SELECT h.id FROM HashtagEntity h WHERE h.lastUsedAt < :date")
+	List<Long> findOldHashtags(@Param("targetDate") LocalDateTime targetDate);
 }
