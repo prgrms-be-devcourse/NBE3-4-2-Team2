fc7f7bb81b6c9000f7ff70a4e7d1b64f0dd35353
ChoiHyunSan <chsan626@gmail.com>
2025-02-04 14:43:33 +0900

    feat: FeedSelector 클래스 테스트 추가

--- backend/src/test/java/com/example/backend/social/feed/implement/FeedSelectorTest.java
+++ backend/src/test/java/com/example/backend/social/feed/implement/FeedSelectorTest.java
@@ -1,5 +1,7 @@
 package com.example.backend.social.feed.implement;
 
+import static com.example.backend.social.feed.constant.FeedConstants.*;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -8,8 +10,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,7 +22,6 @@ import com.example.backend.social.feed.Feed;
 @Transactional
 class FeedSelectorTest {
 
-	private static final Logger log = LoggerFactory.getLogger(FeedSelectorTest.class);
 	@Autowired
 	FeedSelector feedSelector;
 
@@ -68,9 +67,30 @@ class FeedSelectorTest {
 	}
 
 	@Test
-	@DisplayName("팔로잉 게시물들은 시간 순으로 정렬되어 반환된다")
+	@DisplayName("팔로잉 게시물들은 시간 순으로 내림차 정렬되어 반환된다")
 	void t2() {
 		Optional<MemberEntity> memberOpt = memberRepository.findById(1L);
 
+		MemberEntity member = memberOpt.get();
+		List<Feed> byFollower = feedSelector.findByFollower(member, LocalDateTime.now().plusDays(1), null, 10);
+
+		for (int i = 0; i < byFollower.size() - 1; i++) {
+			LocalDateTime front = byFollower.get(i).getPost().getCreateDate();
+			LocalDateTime back = byFollower.get(i + 1).getPost().getCreateDate();
+			Assertions.assertFalse(front.isBefore(back));
+		}
+	}
+
+	@Test
+	@DisplayName("추천 게시물 요청")
+	void t3() {
+		Optional<MemberEntity> memberOpt = memberRepository.findById(1L);
+		MemberEntity member = memberOpt.get();
+
+		List<Feed> recommendFeedList = feedSelector.findRecommendFinder(member, LocalDateTime.now().plusDays(1),
+			LocalDateTime.now().minusDays(1 + RECOMMEND_SEARCH_DATE_RANGE), 10);
+
+		Assertions.assertNotNull(recommendFeedList);
+		Assertions.assertEquals(10, recommendFeedList.size());
 	}
 }
