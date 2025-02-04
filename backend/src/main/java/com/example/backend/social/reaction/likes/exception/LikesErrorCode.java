ab02d939a60320e76f563dd685114513548a08ba
Metronon <bumpercar07@gmail.com>
2025-02-04 18:14:25 +0900

    refactor: 예외 코드 추가 및 Service 검증 로직 추가

--- backend/src/main/java/com/example/backend/social/reaction/likes/exception/LikesErrorCode.java
+++ backend/src/main/java/com/example/backend/social/reaction/likes/exception/LikesErrorCode.java
@@ -15,9 +15,12 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum LikesErrorCode {
+	MEMBER_MISMATCH(HttpStatus.FORBIDDEN, "좋아요를 취소할 권한이 없습니다.", "403"),
 	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "멤버 정보를 찾을 수 없습니다.", "404"),
 	POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시물 정보를 찾을 수 없습니다.", "404"),
-	LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "좋아요 정보를 찾을 수 없습니다.", "404");
+	LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "좋아요 정보를 찾을 수 없습니다.", "404"),
+	POST_MISMATCH(HttpStatus.CONFLICT, "좋아요 정보와 요청 게시물 정보가 다릅니다.", "409"),
+	ALREADY_LIKED(HttpStatus.CONFLICT, "이미 좋아요를 눌렀습니다.", "409");
 
 	final HttpStatus httpStatus;
 	final String message;
