61d81458bac64a36a7159c5082787a1bd58aec44
ChoiHyunSan <chsan626@gmail.com>
2025-02-04 19:59:55 +0900

    feat: FeedController 테스트 코드 작성

--- backend/src/main/java/com/example/backend/social/feed/controller/FeedController.java
+++ backend/src/main/java/com/example/backend/social/feed/controller/FeedController.java
@@ -1,5 +1,9 @@
 package com.example.backend.social.feed.controller;
 
+import static com.example.backend.entity.QMemberEntity.*;
+
+import java.util.Objects;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +14,7 @@ import com.example.backend.global.rs.RsData;
 import com.example.backend.social.feed.dto.FeedListResponse;
 import com.example.backend.social.feed.dto.FeedRequest;
 import com.example.backend.social.feed.service.FeedService;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,11 +31,15 @@ public class FeedController {
 
 	private final FeedService feedService;
 
+	private final JPAQueryFactory queryFactory;
+
 	@GetMapping
 	public ResponseEntity<RsData<FeedListResponse>> findList(
 		@RequestBody FeedRequest request
 	) {
-		Long userId = 1L; // 시큐리티 완성되기 전까지 테스트로 사용할 유저이름
+		Long userId = Objects.requireNonNull(
+			queryFactory.selectFrom(memberEntity).where(memberEntity.username.eq("user1")).fetchOne()).getId();
+		
 		return ResponseEntity.ok()
 			.body(
 				RsData.success(feedService.findList(request, userId), "피드를 성공적으로 반환했습니다.")
