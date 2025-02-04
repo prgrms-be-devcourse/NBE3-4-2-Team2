1e09758f68b35c8b31b32f877e61746c116f2884
sobaman <sobaman522@gmail.com>
2025-02-04 15:50:10 +0900

    feat: 수정 작업을 위한 쿼리 작성
    - postId 를 기반으로 postHashtag 들을 조회한다
    - postId 와 hashtag content 를 기반으로 postHashtag 를 삭제한다

--- backend/src/main/java/com/example/backend/entity/PostHashtagRepository.java
+++ backend/src/main/java/com/example/backend/entity/PostHashtagRepository.java
@@ -23,6 +23,27 @@ public interface PostHashtagRepository extends JpaRepository<PostHashtagEntity, 
 		""")
 	void bulkDeleteByHashtagIds(@Param("hashtagIds") List<Long> hashtagIds);
 
-	@Query("DELETE FROM PostHashtagEntity ph WHERE ph.post.id = :postId")
-	void deleteByPostId(@Param("postId") Long postId);
+	/**
+	 * @author kwak
+	 * @since 2025-02-04
+	 */
+	@Query("""
+		SELECT ph FROM PostHashtagEntity ph
+		JOIN FETCH ph.post p
+		JOIN FETCH ph.hashtag h
+		WHERE ph.post.id = :id
+		""")
+	List<PostHashtagEntity> findPostHashtagByPostId(@Param("id") Long postId);
+
+	/**
+	 * @author kwak
+	 * @since 2025-02-04
+	 */
+	@Query("""
+		DELETE FROM PostHashtagEntity ph
+		WHERE ph.post.id = :postId
+		AND ph.hashtag.content IN :deletedHashtagContents
+		""")
+	void deleteByPostIdAndHashtagContent(
+		@Param("postId") Long postId, @Param("deletedHashtagContents") Set<String> deletedHashtagContents);
 }
