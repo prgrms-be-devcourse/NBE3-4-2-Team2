241a991f1ee103a428a5a5e00eeeda189b46afe2
Metronon <bumpercar07@gmail.com>
2025-02-05 14:54:23 +0900

    refactor: 구조 수정 및 일부 항목 삭제

--- backend/src/main/java/com/example/backend/global/exceptionhandler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/backend/global/exceptionhandler/GlobalExceptionHandler.java
@@ -19,6 +19,8 @@ import com.example.backend.global.error.GlobalErrorCode;
 import com.example.backend.global.exception.GlobalException;
 import com.example.backend.global.rs.ErrorRs;
 import com.example.backend.global.rs.RsData;
+import com.example.backend.social.feed.exception.FeedException;
+import com.example.backend.social.reaction.bookmark.exception.BookmarkException;
 import com.example.backend.social.reaction.likes.exception.LikesException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -112,6 +114,24 @@ public class GlobalExceptionHandler {
 		return ResponseEntity
 			.status(ex.getStatus())
 			.body(response);
+	}
+
+	@ExceptionHandler(BookmarkException.class)
+	public ResponseEntity<RsData<?>> handleBookmarkException(BookmarkException ex) {
+		RsData<?> response = RsData.error(null, ex.getMessage());
+		return ResponseEntity
+			.status(ex.getStatus())
+			.body(response);
+	}
+
+	@ExceptionHandler(FeedException.class)
+	public ResponseEntity<RsData<?>> handleFeedException(FeedException ex, HttpServletRequest request) {
+		return ResponseEntity.status(ex.getErrorCodeIfs().getHttpStatus())
+			.body(RsData.error(ErrorRs.builder()
+				.target(request.getRequestURI())
+				.code(ex.getErrorCodeIfs().getCode())
+				.message(ex.getErrorDescription())
+				.build(), ex.getErrorDescription()));
 	}
 
 	@ExceptionHandler(PostException.class)
