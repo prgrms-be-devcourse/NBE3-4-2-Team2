88fff121c4257a150a46b172813ff55ed221999c
ChoiHyunSan <chsan626@gmail.com>
2025-02-06 11:34:24 +0900

    refactor : Controller HttpMethod를 ResponseStatus 에노테이션으로 수정

--- backend/src/main/java/com/example/backend/social/feed/controller/FeedController.java
+++ backend/src/main/java/com/example/backend/social/feed/controller/FeedController.java
@@ -4,10 +4,11 @@ import static com.example.backend.entity.QMemberEntity.*;
 
 import java.util.Objects;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend.global.rs.RsData;
@@ -34,7 +35,8 @@ public class FeedController {
 	private final JPAQueryFactory queryFactory;
 
 	@GetMapping
-	public ResponseEntity<RsData<FeedListResponse>> findList(
+	@ResponseStatus(HttpStatus.OK)
+	public RsData<FeedListResponse> findList(
 		@RequestBody FeedRequest request
 	) {
 		// 시큐리티 코드를 병합하기 전에 임시로 사용
@@ -42,9 +44,11 @@ public class FeedController {
 				queryFactory.selectFrom(memberEntity).where(memberEntity.username.eq(request.getUsername())).fetchOne())
 			.getId();
 
-		return ResponseEntity.ok()
-			.body(
-				RsData.success(feedService.findList(request, userId), "피드를 성공적으로 반환했습니다.")
-			);
+		return RsData.success(feedService.findList(request, userId), "피드를 성공적으로 반환했습니다.");
 	}
+
+	// 피드 단건 조회
+
+	// 멤버에 대한 조회
+
 }
