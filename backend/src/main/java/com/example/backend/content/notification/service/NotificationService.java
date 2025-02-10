2326a79feb7869bedf9077f963287243d030da84
sobaman <sobaman522@gmail.com>
2025-02-10 19:10:49 +0900

    feat: 알림 서비스에 알림 목록 조회 메서드 추가

--- backend/src/main/java/com/example/backend/content/notification/service/NotificationService.java
+++ backend/src/main/java/com/example/backend/content/notification/service/NotificationService.java
@@ -1,9 +1,17 @@
 package com.example.backend.content.notification.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.backend.content.notification.converter.NotificationConverter;
+import com.example.backend.content.notification.dto.NotificationLikePageResponse;
+import com.example.backend.content.notification.dto.NotificationLikeResponse;
 import com.example.backend.content.notification.exception.NotificationErrorCode;
 import com.example.backend.content.notification.exception.NotificationException;
 import com.example.backend.content.notification.sse.SseConnectionPool;
@@ -25,10 +33,13 @@ public class NotificationService {
 	private final NotificationConverter converter;
 	private final SseConnectionPool sseConnectionPool;
 
+	private static final long THIRTY_DAYS = 30;
+	private static final int PAGE_SIZE = 10;
+
 	@Transactional
 	public void createAndSendNotification(Long memberId, Long postId, NotificationType type, String message) {
 		// 알림 엔티티 생성 및 저장
-		NotificationEntity notificationEntity = NotificationEntity.create(message, memberId, type);
+		NotificationEntity notificationEntity = NotificationEntity.create(message, memberId, type, postId);
 		NotificationEntity notification = notificationRepository.save(notificationEntity);
 
 		// sse 로 실시간 알림 전송
@@ -46,5 +57,20 @@ public class NotificationService {
 			return;
 		}
 		notification.markRead();
+	}
+
+	public NotificationLikePageResponse getNotificationPage(int page, Long memberId) {
+		PageRequest pageRequest = PageRequest.of(page, PAGE_SIZE, Sort.by(Sort.Direction.DESC, "createdDate"));
+		// 알림 목록 조회 (최근 30일)
+		LocalDateTime thirtyDaysAgo = LocalDateTime.now().minusDays(THIRTY_DAYS);
+
+		Page<NotificationEntity> notifications =
+			notificationRepository.findByMemberId(memberId, thirtyDaysAgo, pageRequest);
+
+		Page<NotificationLikeResponse> likeResponse = notifications.map(
+			notification -> converter.toLikeResponse(notification, notification.getTargetId())
+		);
+
+		return converter.toLikePage(likeResponse);
 	}
 }
