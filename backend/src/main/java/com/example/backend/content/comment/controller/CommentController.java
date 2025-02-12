3b7ffb60e47da6828bf05b18f01992df5da09ecd
joonaeng <jylee33286315@gmai.com>
2025-02-12 10:21:38 +0900

    feat : CommentController Post 생성

--- backend/src/main/java/com/example/backend/content/comment/controller/CommentController.java
+++ backend/src/main/java/com/example/backend/content/comment/controller/CommentController.java
@@ -2,12 +2,17 @@ package com.example.backend.content.comment.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.backend.content.comment.dto.CommentCreateRequest;
+import com.example.backend.content.comment.dto.CommentCreateResponse;
 import com.example.backend.content.comment.dto.CommentResponse;
 import com.example.backend.content.comment.service.CommentService;
 
@@ -20,6 +25,10 @@ public class CommentController {
 
 	private final CommentService commentService;
 
+	@PostMapping
+	public ResponseEntity<CommentCreateResponse> createComment(@RequestBody CommentCreateRequest request) {
+		return ResponseEntity.ok(commentService.createComment(request));
+	}
 	/**
 	 * ✅ 특정 게시글의 댓글 목록 조회 (페이징 적용)
 	 */
