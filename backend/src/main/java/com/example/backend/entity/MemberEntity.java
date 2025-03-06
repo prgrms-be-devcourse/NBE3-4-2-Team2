420a66d516bf0373a63954087dc8b92b161b6a68
Metronon <bumpercar07@gmail.com>
2025-03-06 13:44:39 +0900

    refactor: String타입의 상대 이름 가져오도록 변경

--- backend/src/main/java/com/example/backend/entity/MemberEntity.java
+++ backend/src/main/java/com/example/backend/entity/MemberEntity.java
@@ -6,10 +6,6 @@ import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.example.backend.social.exception.SocialErrorCode;
-import com.example.backend.social.exception.SocialException;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -48,7 +44,7 @@ public class MemberEntity extends BaseEntity {
 
 	@Column(nullable = false)
 	@Builder.Default
-	private Long followingCount = 0L; // 팔로위 : 본인을 팔로우중인 인원수
+	private Long followingCount = 0L; // 팔로잉 : 본인을 팔로우중인 인원수
 
 	@OneToMany(mappedBy = "member")
 	@Builder.Default
@@ -56,11 +52,11 @@ public class MemberEntity extends BaseEntity {
 
 	@OneToMany(mappedBy = "receiver") // receiver가 자기 자신 => 나를 팔로우하는
 	@Builder.Default
-	private List<MemberEntity> followerList = new ArrayList<>();
+	private List<FollowEntity> followerList = new ArrayList<>();
 
 	@OneToMany(mappedBy = "sender") // sender가 자기 자신 => 내가 팔로잉하는
 	@Builder.Default
-	private List<MemberEntity> followingList = new ArrayList<>();
+	private List<FollowEntity> followingList = new ArrayList<>();
 
 	@OneToMany(mappedBy = "member")
 	@Builder.Default
@@ -94,45 +90,5 @@ public class MemberEntity extends BaseEntity {
 			authorities.add("ROLE_ADMIN");
 
 		return authorities;
-	}
-
-	@Transactional
-	public void follow(MemberEntity receiver) {
-		// 자기 자신 팔로우 불가
-		if (this.equals(receiver)) {
-			throw new SocialException(SocialErrorCode.CANNOT_PERFORM_ON_SELF, "자기 자신을 팔로우할 수 없습니다.");
-		}
-
-		// 이미 팔로우 중인 대상에 대해 팔로우 시도 불가
-		if (this.followingList.contains(receiver)) {
-			throw new SocialException(SocialErrorCode.ALREADY_EXISTS, "이미 팔로우 상태입니다.");
-		}
-
-		// 팔로우 관계 생성
-		this.followingList.add(receiver);
-		this.followingCount++;
-
-		receiver.followerList.add(this);
-		receiver.followerCount++;
-	}
-
-	@Transactional
-	public void unfollow(MemberEntity receiver) {
-		// 자기 자신 언팔로우 불가
-		if (this.equals(receiver)) {
-			throw new SocialException(SocialErrorCode.CANNOT_PERFORM_ON_SELF, "자기 자신을 언팔로우할 수 없습니다.");
-		}
-
-		// 팔로우 관계가 없는 대상에 대한 언팔로우 요청 불가
-		if (!this.followingList.contains(receiver)) {
-			throw new SocialException(SocialErrorCode.NOT_FOUND, "팔로우 관계를 찾을 수 없습니다.");
-		}
-
-		// 팔로우 관계 제거
-		this.followingList.remove(receiver);
-		this.followingCount--;
-
-		receiver.followerList.remove(this);
-		receiver.followerCount--;
 	}
 }
